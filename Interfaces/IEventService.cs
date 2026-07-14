c249d63e17c474f5636ff9811247180d4dbbd26c
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-07-14 21:28:50 +0300

    Добавлен интерфейс сервиса событий

---
+++ Interfaces/IEventService.cs
@@ -0,0 +1,14 @@
+﻿using EventTicketBookingService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventTicketBookingService.Interfaces
+{
+    public interface IEventService
+    {
+        public List<Event> GetAllEvents();
+        public Event? GetEventById(int id);
+        public Event? CreateEvent([FromBody] Event my_event /*string title, string description*/);
+        public Event UpdateEvent(int id, [FromBody] Event my_event);
+        public Event DeleteEvent(int id);
+    }
+}
