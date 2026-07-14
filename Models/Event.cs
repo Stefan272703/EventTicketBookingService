3fb30f8bfc0e4b1a51331e90c82a03d7e0639b1d
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-07-14 21:29:20 +0300

    Добавлена модель событий

---
+++ Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace EventTicketBookingService.Models
+{
+    public class Event
+    {
+        public required int Id { get; set; }
+        public required string Title {  get; set; }
+        public string Description { get; set; } = string.Empty;
+        public required DateTime StartAt { get;set;  }
+        public required DateTime EndAt { get;set;  }
+    }
+}
