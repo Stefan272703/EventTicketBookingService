8677c26496712836db7f6aea0bc4c4ccb8c5b6ac
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-07-15 12:54:46 +0300

    Модифицирован Event. Добавлен аттрибут валидации времени мероприятия. Конец мероприятия должен быть позже начала мероприятия

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,17 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EventTicketBookingService.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventTicketBookingService.Models
 {
+    [NotAfterStartAtTime]
     public class Event
     {
         [Required(ErrorMessage = "ID обязательное для заполнения")]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Title обязательное для заполнения")]
-        public string Title {  get; set; }
-        public string Description { get; set; } = string.Empty;
+        public string? Title { get; set; }
+
+        public string? Description { get; set; } = string.Empty;
+
         [Required(ErrorMessage = "StartAt обязательное для заполнения")]
-        public DateTime StartAt { get;set;  }
+        public DateTime StartAt { get; set; }
+
         [Required(ErrorMessage = "EndAt обязательное для заполнения")]
-        public DateTime EndAt { get;set;  }
+        public DateTime EndAt { get; set; }
     }
 }
