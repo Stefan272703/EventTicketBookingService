923559b11b430309681870d5f24b7281be9f9954
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-08-11 13:38:24 +0300

    Интерфейс бронирования

---
+++ EventTicketBookingService/Interfaces/IBookingService.cs
@@ -0,0 +1,13 @@
+﻿using EventTicketBookingService.Models;
+
+namespace EventTicketBookingService.Interfaces
+{
+    public interface IBookingService
+    {
+        // Создание брони для указанного события
+        public BookingResponse? CreateBookingAsync(int eventId);
+
+        // Получение брони по идентификатору
+        public Booking? GetBookingByIdAsync(int bookingId);
+    }
+}
