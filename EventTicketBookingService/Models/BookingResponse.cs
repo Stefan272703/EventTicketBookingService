6490f7f586f8229d39c423c2ea1acabbca54cf4e
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-08-11 13:42:33 +0300

    Добавлена модель для тела ответа DTO

---
+++ EventTicketBookingService/Models/BookingResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventTicketBookingService.Models
+{
+    public class BookingResponse
+    {
+        public int Id { get; set; }
+
+        public int EventId { get; set; }
+
+        public BookingStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
