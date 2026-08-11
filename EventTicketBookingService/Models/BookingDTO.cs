332eea1e9f575973a0211823905cbd05cbd85c82
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-08-11 13:40:16 +0300

    Удален

--- EventTicketBookingService/Models/BookingDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EventTicketBookingService.Models
-{
-    public class BookingDTO
-    {
-        public BookingStatus Status { get; init; } = BookingStatus.Pending;
-        public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-    }
-}
