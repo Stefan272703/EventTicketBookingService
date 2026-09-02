7cf8a4c21591a45c287ecb94327f4df5a0093e76
Stefan272703 <stepan.petrovskiy@yandex.ru>
2026-09-02 16:00:15 +0300

    Поменял местами аттрибуты валидаии Event и EventDTO

--- EventTicketBookingService/Models/Event.cs
+++ EventTicketBookingService/Models/Event.cs
@@ -3,20 +3,18 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EventTicketBookingService.Models
 {
-    [NotAfterStartAtTime]
     public class Event
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Title обязательное для заполнения")]
         public string? Title { get; set; }
 
         public string? Description { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "StartAt обязательное для заполнения")]
         public DateTime StartAt { get; set; }
 
-        [Required(ErrorMessage = "EndAt обязательное для заполнения")]
         public DateTime EndAt { get; set; }
+
+
     }
 }
